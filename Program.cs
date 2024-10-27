ca58e299767c5c61e06a9b0f7bd368bbc9ea3a0f
htorres38 <hjtorres1@buffs.wtamu.edu>
2024-10-26 23:40:09 -0500

    Homework8 upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Homework8
+{
+    class Program{
+    static void Main(string[] args)
+    {
+        Customer cus1 = new Customer(customerName:"Alice", customerAge:33, customerCity:"Amarillo", customerCredit:198.5);
+        Customer cus2 = new Customer(customerName:"Bob", customerAge:23, customerCity:"Amarillo", customerCredit:226);
+        Customer cus3 = new Customer(customerName:"Cathy", customerAge:45, customerCity:"Amarillo", customerCredit:89.0);
+        Customer cus4 = new Customer(customerName:"David", customerAge:58, customerCity:"Amarillo", customerCredit:198.5);
+        Customer cus5 = new Customer(customerName:"Jack", customerAge:28, customerCity:"Canyon", customerCredit:561.6);
+        Customer cus6 = new Customer(customerName:"Tom", customerAge:36, customerCity:"Canyon", customerCredit:98.4);
+        Customer cus7 = new Customer(customerName:"Tony", customerAge:24, customerCity:"Canyon", customerCredit:18.5);
+        Customer cus8 = new Customer(customerName:"Sam", customerAge:35, customerCity:"Canyon", customerCredit:228.3);
+        
+        Customer[] customer_list = {cus1, cus2, cus3, cus4, cus5, cus6, cus7, cus8};
+
+
+        //Q1
+        Customer.TotalCredits(customer_list);
+        //Q2
+        Customer.AmarilloAverageAge(customer_list);
+        //Q3
+        Customer.CanyonAge(customer_list);
+    }
+    }
+}
